cdc26bb4ec69c077b0f83e561a2bf4b6ec15a6c1
zhangqian00 <zq_674360351@163.com>
2020-04-23 13:04:42 +0800

    每天一道算法题

---
+++ 20200423.js
@@ -0,0 +1,33 @@
+/*给定一个整数数组，判断是否存在重复元素。
+
+如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+
+ 
+
+示例 1:
+
+输入: [1,2,3,1]
+输出: true
+示例 2:
+
+输入: [1,2,3,4]
+输出: false
+示例 3:
+
+输入: [1,1,1,3,3,4,3,2,4,2]
+输出: true
+*/
+
+var containsDuplicate = function(nums) {
+    let len = nums.length;
+    let flag = false;
+    for(let i=0;i<len;i++){
+        for(let k=i+1;k<len;k++){
+            if(nums[i]===nums[k]){
+                flag = true;
+                break;
+            }
+        }
+    }
+    return flag;
+};
