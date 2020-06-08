539d36487dcae4fcf0cd471ae18d294f9475dc4f
zhangqian00 <zq_674360351@163.com>
2020-06-08 13:03:52 +0800

    每天一道算法题

--- 009-两数之和.js
+++ 009-两数之和.js
@@ -29,3 +29,14 @@ var twoSum = function(nums, target) {
         }
     }
 };
+var twoSum = function(nums, target) {
+    let len = nums.length
+    for(let i=0;i<len;i++){
+        for(let j=i+1;j<len;j++){
+            if(nums[i]+nums[j]===target){
+                return [i,j]
+            }
+        }
+    }
+    return []
+};
