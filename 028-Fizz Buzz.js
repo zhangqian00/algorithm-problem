010ee8a020107b93f99b9c2751ca90ab77c9432b
zhangqian00 <zq_674360351@163.com>
2020-06-28 17:26:11 +0800

    每天一道算法题

--- 028-Fizz Buzz.js
+++ 028-Fizz Buzz.js
@@ -35,9 +35,24 @@ n = 15,
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-	let arr = [];
-	for(let i=1;i<=n;i++){
-		arr.push((i%3===0&&i%5===0)?"FizzBuzz":i%3===0?"Fizz":i%5===0?"Buzz":i.toString());
-	}
-	return arr;
+    let arr = [];
+    for(let i=1;i<=n;i++){
+        arr.push((i%3===0&&i%5===0)?"FizzBuzz":i%3===0?"Fizz":i%5===0?"Buzz":i.toString());
+    }
+    return arr;
+};
+var fizzBuzz = function(n) {
+    let arr = []
+    for(let i=1;i<=n;i++){
+        if(i%3===0&&i%5===0){
+            arr.push('FizzBuzz')
+        }else if(i%3===0){
+            arr.push('Fizz')
+        }else if(i%5===0){
+            arr.push('Buzz')
+        }else {
+            arr.push(i.toString())
+        }
+    }
+    return arr
 };
