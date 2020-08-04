d4307685c10a5f79d1a9af16266869d687bc053c
zhangqian00 <zq_674360351@163.com>
2020-08-04 12:58:46 +0800

    每天一道算法题

---
+++ 059-两个数组的交集.js
@@ -0,0 +1,34 @@
+/*
+给定两个数组，编写一个函数来计算它们的交集。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2]
+示例 2：
+
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[9,4]
+ 
+
+说明：
+
+输出结果中的每个元素一定是唯一的。
+我们可以不考虑输出结果的顺序。
+ */
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    let res = []
+    for(let i=0;i<nums1.length;i++){
+        if(nums2.indexOf(nums1[i])!==-1){
+            res.push(nums1[i])
+        }
+    }
+    return Array.from(new Set(res))
+};
