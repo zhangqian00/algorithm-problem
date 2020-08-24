b68a272dfde1daf2c2510a61d52391ad45e030c5
zhangqian00 <zq_674360351@163.com>
2020-08-24 10:41:38 +0800

    LeetCode算法题

---
+++ 076-最大连续1的个数.js
@@ -0,0 +1,20 @@
+/*
+给定一个二进制数组， 计算其中最大连续1的个数。
+
+示例 1:
+
+输入: [1,1,0,1,1,1]
+输出: 3
+解释: 开头的两位和最后的三位都是连续1，所以最大连续1的个数是 3.
+注意：
+
+输入的数组只包含 0 和1。
+输入数组的长度是正整数，且不超过 10,000。
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    return Math.max.apply(null,nums.join('').split('0').map((el)=>el.length))
+};
